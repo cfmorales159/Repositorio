1c8fe88cd025a7b443a67995fee47a911e074aab
Claudio F Morales <claudiomorales09@gmail.com>
2026-03-05 15:56:41 -0300

    modificacion NR

--- Chapra/7_newton_raphson.py
+++ Chapra/7_newton_raphson.py
@@ -1,20 +1,28 @@
 import numpy as np
+# OBSERVAR SE TIENEN DOS EJEMPLOS DE FUNCIONES, CONVERGE OTRA DIVERGE
+#def f(x):
+    # Función del ejemplo del Chapra: f(x) = e^(-x) - x
+   # return np.exp(-x) - x
 
 def f(x):
     # Función del ejemplo del Chapra: f(x) = e^(-x) - x
-    return np.exp(-x) - x
+    return x**2-2*x+4
 
-def df(x):
+#def df(x):
     # DERIVADA: Aquí el alumno debe aplicar lo visto en clase
     # f'(x) = -e^(-x) - 1
-    return -np.exp(-x) - 1
+   # return -np.exp(-x) - 1
+def df(x):
+    # DERIVADA: Aquí el alumno debe aplicar lo visto en clase
+    #
+    return 2*x-2
 
 def newton_raphson(x0, tol, max_iter):
     print(f"{'Iter':<10} {'xi':<15} {'error (%)':<15}")
     print("-" * 40)
     
     xi = x0
-    for i in range(max_iter):
+    for i in range(max_iter): # esta sentencia pone freno a posible bucle
         # --- PARTE A COMPLETAR POR EL ALUMNO ---
         # Aplicar la fórmula: x_i+1 = x_i - f(x_i) / f'(x_i)
         
@@ -42,7 +50,7 @@ def newton_raphson(x0, tol, max_iter):
     return xi
 
 # Parámetros iniciales
-x_inicial = 2 # se elije adivinando por donde podría estar la raíz
+x_inicial = 0 # se elije adivinando por donde podría estar la raíz
 tolerancia = 0.0001 # 0.0001%
 iteraciones = 10
 
