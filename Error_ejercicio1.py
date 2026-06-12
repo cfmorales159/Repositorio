9e01e8b952357427867df647a909ba1b9280eb2d
Claudio F Morales <claudiomorales09@gmail.com>
2026-06-12 17:13:24 -0300

    segundo commit Chapra

---
+++ Error_ejercicio1.py
@@ -0,0 +1,12 @@
+# Declaracion de variables
+a = 0.1
+b = 0.2
+resultado_esperado = 0.3
+# Mostrar el resultado de la comparación 
+print("0.1+0.2 es exactamente igual a 0.3?")
+print(a+b==resultado_esperado)
+# separador
+print("^"*40)
+# Mostar el valor que ve python
+print(f"Valor real de la cuenta en memoria: {a +b:.17f}")
+print(f"Valor real 0.3 leido por Python en memoria: {resultado_esperado:.17f}")
